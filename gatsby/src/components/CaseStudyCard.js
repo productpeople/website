3a06a2d18fc342610a92de063ce429e1d6837a62
Callum Macdonald <git@callum-macdonald.com>
2020-02-13 18:13:05 +0100

    Linting cleanup.

--- gatsby/src/components/CaseStudyCard.js
+++ gatsby/src/components/CaseStudyCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 // import { withPrefix } from "gatsby";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActionArea from "@material-ui/core/CardActionArea";

ecbc8969c7650262b12a5be03c5f7d5dfe64e90a
Callum Macdonald <git@callum-macdonald.com>
2020-02-13 14:57:33 +0100

    Prettier.

--- gatsby/src/components/CaseStudyCard.js
+++ gatsby/src/components/CaseStudyCard.js
@@ -1,47 +1,47 @@
-import React from 'react';
+import React from "react";
 // import { withPrefix } from "gatsby";
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/styles/withStyles';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/styles/withStyles";
 
 const styles = theme => ({
   root: {
-    borderRadius: '6px 6px 6px 6px',
-    width: '331px',
-    margin: '20px'
+    borderRadius: "6px 6px 6px 6px",
+    width: "331px",
+    margin: "20px"
   },
   cardHeader: {
-    height: '100px',
-    background: '#000000',
-    color: '#FFFFFF',
-    textAlign: 'center',
-    padding: '19px'
+    height: "100px",
+    background: "#000000",
+    color: "#FFFFFF",
+    textAlign: "center",
+    padding: "19px"
   },
   cardTitle: {
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontSize: '24px',
-    lineHeight: '32px',
-    textAlign: 'center',
-    letterSpacing: '1px',
-    color: '#FFFFFF'
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: "24px",
+    lineHeight: "32px",
+    textAlign: "center",
+    letterSpacing: "1px",
+    color: "#FFFFFF"
   },
   cardSubtitle: {
     fontFamily: theme.monoFont,
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontSize: '12px',
-    lineHeight: '32px',
-    textAlign: 'center',
-    letterSpacing: '0.5px',
-    color: '#FFFFFF'
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: "12px",
+    lineHeight: "32px",
+    textAlign: "center",
+    letterSpacing: "0.5px",
+    color: "#FFFFFF"
   },
   cardMedia: {
-    objectFit: 'cover'
+    objectFit: "cover"
   }
 });
 

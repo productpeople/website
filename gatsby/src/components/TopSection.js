30cb3f069b3b0ee1e8bd4a8c45ce45365baf9e86
Sean Coley <seanwcoley@gmail.com>
2020-02-13 17:12:00 +0100

    Update TopSection

--- gatsby/src/components/TopSection.js
+++ gatsby/src/components/TopSection.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import ButtonBig from './ButtonBig';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/styles/withStyles';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import ButtonBig from "./ButtonBig";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/styles/withStyles";
 
 const styles = theme => ({
-  root: {
-    // width: '100%',
-    // height: '100%'
-  },
+  root: {},
   cardRoot: {
-    padding: '140px 0px'
+    padding: "140px 0px",
+    background: "#FFFFFF",
+    boxShadow: "0px 2px 50px rgba(0, 0, 0, 0.02) !important",
+    borderRadius: 6,
+    maxHeight: 580
   },
   cardContent: {
-    margin: 'auto'
+    margin: "auto"
   },
   title: {
-    margin: '0 auto',
-    textAlign: 'center',
-    maxWidth: '700px',
+    margin: "0 auto",
+    textAlign: "center",
+    maxWidth: "700px",
     fontWeight: 700,
-    fontSize: '56px',
-    lineHeight: '70px',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: '30px',
-      lineHeight: '40px',
-      marginBottom: '72px'
+    fontSize: "56px",
+    lineHeight: "70px",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "30px",
+      lineHeight: "40px",
+      marginBottom: "72px"
     }
   }
 });
@@ -35,7 +36,7 @@ const TopSection = props => {
   const { classes, titleText, buttonText } = props;
   return (
     <div className={classes.root}>
-      <Card className={classes.cardRoot}>
+      <Card classes={{ root: classes.cardRoot }} elevation={0}>
         <CardContent className={classes.cardContent}>
           <Typography classes={{ root: classes.title }} component="h1">
             {titleText}

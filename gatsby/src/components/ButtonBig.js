905a82a41afda7e8bd11997d784d8cde875b6688
Callum Macdonald <git@callum-macdonald.com>
2020-02-13 20:48:52 +0100

    Remove !important. Prettier.

--- gatsby/src/components/ButtonBig.js
+++ gatsby/src/components/ButtonBig.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import withStyles from '@material-ui/styles/withStyles';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import withStyles from "@material-ui/styles/withStyles";
 
 const styles = {
   rootEl: {
-    background: '#323FD0 !important',
-    border: '2px solid #323FD0 !important',
-    borderRadius: '20px !important',
-    height: '48px',
-    width: '232px',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: '48px'
+    background: "#323FD0",
+    border: "2px solid #323FD0",
+    borderRadius: "20px",
+    height: "48px",
+    width: "232px",
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "48px"
   },
   text: {
     // fontFamily: "Red Hat Display",
-    textTransform: 'none',
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontSize: '18px',
-    lineHeight: '24px',
-    color: '#E6E7E9'
+    textTransform: "none",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: "18px",
+    lineHeight: "24px",
+    color: "#E6E7E9"
   }
 };
 

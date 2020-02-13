bc5a3742ecb73c4f746d0ba707e2935c96fd6c96
Sean Coley <seanwcoley@gmail.com>
2020-02-13 17:13:01 +0100

    Fix Prettier

--- gatsby/src/components/TextSectionSmallTitle.js
+++ gatsby/src/components/TextSectionSmallTitle.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import makeStyles from '@material-ui/styles/makeStyles';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: '20px',
-    background: 'transparent'
+    paddingRight: "20px",
+    paddingLeft: "20px",
+    marginBottom: "60px !important",
+    background: "transparent"
   },
   title: {
     fontFamily: theme.monoFont,
-    fontStyle: 'normal',
+    fontStyle: "normal",
     fontWeight: 400,
-    fontSize: '12px',
-    lineHeight: '30px',
-    textAlign: 'center',
-    letterSpacing: '4px',
-    color: '#000000',
-    marginTop: '120px',
-    marginBottom: '20px'
+    fontSize: "12px",
+    lineHeight: "30px",
+    textAlign: "center",
+    letterSpacing: "4px",
+    color: "#000000",
+    marginTop: "120px",
+    marginBottom: "20px"
   },
   text: {
-    marginTop: '20px',
-    fontStyle: 'normal',
+    marginTop: "20px",
+    fontStyle: "normal",
     fontWeight: 400,
-    fontSize: '20px',
-    lineHeight: '26px',
-    textAlign: 'center',
-    letterSpacing: '0.833333px'
+    fontSize: "20px",
+    lineHeight: "26px",
+    textAlign: "center",
+    letterSpacing: "0.833333px"
   }
 }));
 
@@ -35,7 +37,7 @@ const TextSectionSmallTitle = props => {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       <Typography classes={{ root: classes.title }}>{title}</Typography>
       <Typography classes={{ root: classes.text }}>{text}</Typography>
     </div>

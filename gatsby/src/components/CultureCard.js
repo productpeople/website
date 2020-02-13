a55decfa08da466a3c37f5049df9d8cf5e2fbd8f
Sean Coley <seanwcoley@gmail.com>
2020-02-13 17:13:42 +0100

    WIP CultureCard updates

--- gatsby/src/components/CultureCard.js
+++ gatsby/src/components/CultureCard.js
@@ -1,57 +1,69 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import makeStyles from '@material-ui/styles/makeStyles';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
   cardRoot: {
-    width: '331px',
-    height: '412px',
-    background: '#FFFFFF',
-    boxShadow: '0px 2px 50px rgba(0, 0, 0, 0.05)',
-    borderRadius: '5px',
-    margin: '20px',
-    overflow: 'visible',
-    display: 'flex',
-    textAlign: 'center'
+    width: "331px",
+    height: "412px",
+    background: "#FFFFFF",
+    boxShadow: "0px 2px 50px rgba(0, 0, 0, 0.05)",
+    borderRadius: "5px",
+    margin: "20px",
+    overflow: "visible",
+    display: "flex",
+    textAlign: "center"
+  },
+  cardMedia: {
+    height: 33,
+    width: 35,
+    margin: "0 auto"
   },
   cardContent: {
-    height: '100%',
-    width: '100%',
-    padding: '36px 72px'
+    height: "100%",
+    width: "100%",
+    padding: "36px 72px"
   },
   title: {
     fontFamily: theme.typography.fontFamily,
     fontWeight: 700,
-    fontSize: '24px',
-    lineHeight: '32px',
-    letterSpacing: '1px',
-    marginTop: '30px'
+    fontSize: 24,
+    lineHeight: "32px",
+    letterSpacing: 1,
+    marginTop: 24
   },
   listElement: {
-    marginTop: '32px'
+    marginTop: "32px"
   },
   listItem: {
     padding: 0,
-    textAlign: 'center'
+    textAlign: "center"
   },
   listItemText: {
-    fontSize: '18px !important',
-    lineHeight: '32px !important',
+    fontSize: "18px !important",
+    lineHeight: "32px !important",
     padding: 0
   }
 }));
 
 const CultureCard = props => {
-  const { children } = props;
+  const { children, imageURL, altText } = props;
   const classes = useStyles();
   return (
-    <Card className={classes.cardRoot}>
+    <Card classes={{ root: classes.cardRoot }} elevation={0}>
       <CardContent classes={{ root: classes.cardContent }}>
+        <CardMedia
+          className={classes.cardMedia}
+          component="img"
+          alt={altText}
+          src={imageURL}
+        />
         {children}
       </CardContent>
     </Card>

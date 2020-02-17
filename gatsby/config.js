4fb4d9f033c838a76e1926599f96fe27da2d3d0d
Sean Coley <seanwcoley@gmail.com>
2020-02-17 17:25:14 +0100

    Add link paths to config

--- gatsby/config.js
+++ gatsby/config.js
@@ -2,5 +2,8 @@ module.exports = {
   title: "Product People",
   maxWidth: 1224,
   background: "#F2F2F2",
-  scheduleACallURL: "https://calendly.com/mirelamus"
+  scheduleACallURL: "https://calendly.com/mirelamus",
+  imprintPath: "/imprint",
+  termsPath: "/terms",
+  privacyPath: "/privacy"
 };

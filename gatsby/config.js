ea6e78ad9fb4772751797efc8d6c3f90e960f460
Mirela Mus <mirelamustata1@gmail.com>
2020-12-22 21:27:01 +0100

    replaced calendar link from mirelamus to productpeople/client

--- gatsby/config.js
+++ gatsby/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Product People",
   maxWidth: 1224,
   background: "#F2F2F2",
-  scheduleACallURL: "https://calendly.com/mirelamus",
+  scheduleACallURL: "https://calendly.com/productpeople/client",
   imprintPath: "/imprint",
   termsPath: "/terms",
   privacyPath: "/privacy"

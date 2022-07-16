49ba060518a250e01c1c39f2efbbbcaac69dc9d1
mashallm <38189122+mashallm@users.noreply.github.com>
2022-07-16 18:06:01 +0500

    cookie banner debug

--- gatsby/src/components/Page.js
+++ gatsby/src/components/Page.js
@@ -51,11 +51,11 @@ const Page = props => {
       </Grid>
       <CookieBanner
         message="Cookies help us to improve. By continuing to browse the site you're accepting our cookies."
-        //dismissOnScroll={true}
+        dismissOnScroll={false}
         //onAccept={() => {}}
         //cookie="user-has-accepted-cookies"
         styles={{
-          banner: { background: '#F5F6FF', position: 'fixed', bottom: 0, color: 'white', padding: '24px 32px', height: '60px' },
+          banner: { background: '#F5F6FF', position: 'fixed', bottom: 0, color: 'white',  padding: '32px 32px', height: '60px' },
           message: { fontWeight: 500, color: '#000000', font: 'Red Hat Display' },
           button: {display:"none"}
         }}
